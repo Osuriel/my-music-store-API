c922966a578bc57358383f9301e7a175ff2f0092
Omar Suriel <omarsuriel@icloud.com>
2021-11-24 15:18:13 -0500

    saving user favorites works

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,50 +1,38 @@
+const mongoose = require('mongoose');
 
 
-var users = [
-  {
-    id: '111',
-    email: 'john@email.com',
-    name: 'John',
-    lastName: 'Smith',
-    password: 'password',
-    favoriteItems: ['234']
-  }
-];
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  email: { type: String, required: true },
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+  favoriteItems: { type: Array, default: []}
+});
 
-const logIn = (req, res) => {
+const UserModel = mongoose.model('User', userSchema);
+
+const logIn = async (req, res) => {
   console.log('req.body: ', req.body);
 
   const { email, password } = req.body;
 
-  const userFound = users.find(user => {
-    if(user.email === email && user.password === password){
-      return true;
-    }
-    
-    return false;
-  });
-
-  console.log('userFound: ', userFound);
-
+  const userFound = await UserModel.findOne({email, password});
+  
   res.send(userFound);
 };
 
-const editUserFavorites = (req, res) => {
-  console.log('req.body: ', req.body);
+const editUserFavorites = async (req, res) => {
 
   const { userId, favorites } = req.body;
 
-  // fetchingData from imaginary database
+  const user = await UserModel.findOneAndUpdate(
+    {id: userId},
+    { favoriteItems: favorites },
+    { returnDocument: 'after'}
+  );
 
-  const user = users.find(user => user.id === userId);
-
-  const arrayOfOtherUsers = users.filter(user => user.id !== userId);
-
-  const userWithUpdatedFavorites = {...user, favoriteItems: favorites};
-
-  users = [...arrayOfOtherUsers, userWithUpdatedFavorites]
-
-  res.send(userWithUpdatedFavorites);
+  res.send(user);
 };
 
 const UserService = {
